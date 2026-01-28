86a96d64b957775ed46e21c7d52d58e089f852e3
harry pattani <harrypattani15@gmail.com>
2026-01-28 15:13:50 +0530

    groq api usage chat

---
+++ ChatModels/1_chatmodel_groq_api.py
@@ -0,0 +1,11 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGroq(
+    model="llama-3.1-8b-instant"
+)
+
+response = llm.invoke("What is the capital of India?")
+print(response.content)
