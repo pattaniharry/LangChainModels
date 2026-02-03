5385c0b2333ce2768a2d39b62dcef0d77b94dc2f
harry pattani <harrypattani15@gmail.com>
2026-02-03 14:52:41 +0530

    parallel chaining and runable parallel

---
+++ LangChain_Chains/Sequential_chain.py
@@ -0,0 +1,40 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate ,PromptTemplate
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+
+llm = ChatGroq(
+    model="llama-3.1-8b-instant",
+)
+
+
+
+
+#we are using prompt and chaining first one we will ask for detailed prompt 
+
+template1 = PromptTemplate (
+    template  = "provide a detailed explanation of the following topic: {topic}",
+    input_variables = ["topic"]
+)
+
+#second one we will ask for summary using that detailed prompt 
+
+
+template2 = PromptTemplate (
+    template  = "Provide only 5 line summary from given data: {text}",
+    input_variables = ["text"]
+)
+
+parser = StrOutputParser()
+
+
+chain = template1 | llm | parser | template2 | llm | parser
+
+result = chain.invoke({"topic": "Budget 2026"})
+
+print (result)
+
+chain.get_graph().print_ascii()  
---
+++ LangChain_Chains/parallel_chain.py
@@ -0,0 +1,45 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.prompts import  PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel
+
+
+load_dotenv()
+
+model1 = ChatGroq(
+    model = "llama-3.1-8b-instant",
+)
+
+prompt1 = PromptTemplate(
+    template = "generate short and simple notes form the following text \n {text}",
+    input_variables=["text"]
+)
+
+prompt2 = PromptTemplate(
+    template = "generate a quiz with 5 questions from the following notes \n {text}",
+    input_variables=["text"]
+)
+
+prompt3 = PromptTemplate(
+    template = "merge the provided notes and quiz into a single study guide \n Notes: {notes} \n Quiz: {quiz}",
+    input_variables=["notes", "quiz"]
+)
+
+
+parser = StrOutputParser()
+
+parallel_chain = RunnableParallel(
+    {
+        "notes": prompt1 | model1 | parser,
+        "quiz": prompt2 | model1 | parser
+    }
+)
+
+merge_chain = prompt3 | model1 | parser
+
+chain = parallel_chain | merge_chain
+
+result = chain.invoke({'text': "Photosynthesis is the process by which green plants and some other organisms use sunlight to synthesize foods with the help of chlorophyll. It involves the conversion of carbon dioxide and water into glucose and oxygen. This process is essential for the survival of life on Earth as it provides oxygen for respiration and organic compounds for food."})
+
+print (result)
