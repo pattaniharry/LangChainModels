bc4424e5eabf3a2c00b29a8f5e6a25305d2e6a39
harry pattani <harrypattani15@gmail.com>
2026-02-06 14:51:39 +0530

    runnable lambda

---
+++ LangChain_Runnables/runnable_lambda.py
@@ -0,0 +1,11 @@
+from langchain_core.runnables import RunnableLambda
+
+def word_counter(text):
+    return len(text.split())
+
+runnable_word_counter = RunnableLambda(word_counter)
+
+result = runnable_word_counter.invoke("this is a simple string , is it ?")
+
+
+print(result)
